a1a86528c394c4f16c7eed8bebb33135c61243de
Pavel Stebunov <pavel.stebunov@gmail.com>
2024-06-13 18:39:26 +0300

    done

---
+++ src/main/java/ru/kata/spring/boot_security/demo/service/UserService.java
@@ -0,0 +1,23 @@
+package ru.kata.spring.boot_security.demo.service;
+
+
+import ru.kata.spring.boot_security.demo.model.User;
+
+import java.util.List;
+
+public interface UserService {
+    List<User> listUsers();
+
+    void add(User user);
+
+//    void delete(User user);
+
+    void deleteById(Long id);
+
+    void update(User user);
+
+    User findById(Long id);
+
+    User findByUsername(String username);
+
+}
